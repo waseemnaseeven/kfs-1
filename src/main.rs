71022123aad83d18f503e98cc0c5ff8eab673e67
wnaseeve <waseem.naseeven@gmail.com>
2024-09-21 16:48:01 +0200

    implementing test runner, qemu exitcode

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,82 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where 
+    T: Fn(),
+{
+    fn run(&self){
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-// Writing directly in VGA buffer "Hello, World!"
-// 'b' prefix = byte string, every character mean an ASCII character
-static HELLO: &[u8] = b"Hello, World!";
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // 0xb8000 : adress of VGA buffer, espace mem dedie au display text sur system x_86
-    // *mut u8 : mutable pointer to an byte (octet)
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    // enumerate() give index i and a reference to byte for every character
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // unsecure access to the memory
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    // panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
-// [unstable]
-// build-std-features = ["compiler-builtins-mem"]
-// build-std = ["core", "compiler_builtins"]
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success)
+}
 
-// [build]
-// target = "x86_64-kfs-1.json"
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1,1);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
